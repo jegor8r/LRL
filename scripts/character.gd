e61e6a65f1e53cc991ef36df0f2ea6150a5e43aa
jegor8r <150127991+jegor8r@users.noreply.github.com>
2026-06-09 23:48:33 +0300

    Made default personage, animation, walking fucntion

---
+++ scripts/character.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+@export var speed = 150
+
+# получаем узел AnimatedSprite2D
+@onready var sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+	
+	# считываем ввод
+	if Input.is_action_pressed("ui_up"):
+		direction.y = -1
+	if Input.is_action_pressed("ui_down"):
+		direction.y = 1
+	if Input.is_action_pressed("ui_left"):
+		direction.x = -1
+	if Input.is_action_pressed("ui_right"):
+		direction.x = 1
+	
+	# двигаем персонажа
+	velocity = direction * speed
+	move_and_slide()
+	
+	# анимации
+	if direction == Vector2.ZERO:
+		sprite.play("Idle")
+	elif direction.y < 0:
+		sprite.play("Walk_up")
+	elif direction.y > 0:
+		sprite.play("Walk_down")
+	elif direction.x < 0:
+		sprite.play("Walk_left")
+	elif direction.x > 0:
+		sprite.play("Walk_right")
