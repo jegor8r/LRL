47a84d138f66e20042cd3685a032e6c1387df8a8
jegor8r <150127991+jegor8r@users.noreply.github.com>
2026-06-12 23:03:11 +0300

    Added comments, upgraded npc code and corected some mistakes

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -7,50 +7,57 @@ extends CharacterBody2D
 
 var target_position = Vector2.ZERO
 var is_running = false
+
+#Функция выбора рандомной точки
 func pick_random_target():
 	target_position = global_position + Vector2(
 		randf_range(-300, 300),
 		randf_range(-300, 300)
 	)
+#Логика ходьбы и выбора точки
 func move_to_target():
 	var direction = (target_position - global_position).normalized()
 	var distance = global_position.distance_to(target_position)
 	if distance < 10:
 		pick_random_target()
-		return
-		var current_speed = run_speed if is_running else speed
-		velocity = direction * current_speed
-		move_and_slide()
-		update_animation(direction)
+		return #После ретурна функция возращается
+	var current_speed = run_speed if is_running else speed
+	velocity = direction * current_speed
+	move_and_slide()
+	update_animation(direction)
+#Анимация ходьбы и ожидания
 func update_animation(direction):
 	if velocity == Vector2.ZERO:
 		sprite.play("Idle")
 	elif direction.y < 0:
-		sprite.play("Walk_left")
-	elif  direction.y > 0:
-		sprite.play("walk_right")
-	elif direction.x > 0:
 		sprite.play("Walk_up")
-	elif direction.x < 0:
+	elif  direction.y > 0:
 		sprite.play("Walk_down")
-		
-func _ready():
-	pick_random_target()
-func _physics_process(delta):
-	move_to_target()
-
+	elif direction.x > 0:
+		sprite.play("Walk_right")
+	elif direction.x < 0:
+		sprite.play("Walk_left")
+#Функция получение урона
 func take_damage(amount):
-	health -= amount
-	blink()
-	is_running = true
-	pick_random_target()
-	await get_tree().create_timer(3.0).timeout
-	is_running = false
-	if health <= 0:
-		queue_free()
+	if health - amount:
+		blink()
+		health -= amount
+		is_running = true
+		pick_random_target()
+		await get_tree().create_timer(3.0).timeout
+		is_running = false
+		if health <= 0:
+			queue_free()
+#Функция моргания
 func blink():
 	for i in range(3):
 		sprite.modulate = Color(1, 0, 0, 0.5)
 		await  get_tree().create_timer(0.5).timeout
 		sprite.modulate = Color(1, 1, 1)
 		await get_tree().create_timer(0.5).timeout
+#Вызываем функции
+func _ready():
+	await get_tree().create_timer(0.5).timeout
+	pick_random_target()
+func _physics_process(delta):
+	move_to_target()
